3547eb2b67296dfb156cf14b77a987fcc407edfc
Rong Ying <kohrongying@gmail.com>
2019-12-25 18:32:30 +0800

    Add styling, change default starting to 0 and enable deselect loser

--- src/App.svelte
+++ src/App.svelte
@@ -1,22 +1,21 @@
 <script>
-	export let name;
 	let gameStatus = 'WELCOME';
 	let players = [
 		{
 			name: 'PLAYER1',
-			score: 20000,
+			score: 0,
 		},
 		{
 			name: 'PLAYER2',
-			score: 20000,
+			score: 0,
 		},
 		{
 			name: 'PLAYER3',
-			score: 20000,
+			score: 0,
 		},
 		{
 			name: 'PLAYER4',
-			score: 20000,
+			score: 0,
 		},
 	]
 
@@ -37,7 +36,11 @@
 	}
 
 	const setLoser = (index) => () => {
-		loserIndex = index
+		if (loserIndex == index) {
+			loserIndex = null
+		} else {
+			loserIndex = index
+		}
 	}
 
 	const computeResult = () => {
@@ -73,6 +76,7 @@
 
 <main>
 	{#if gameStatus == 'WELCOME'}
+		<h1>Mahjong Scores</h1>
 		<button on:click={changeStatus('GAME_INTIALISED')}>
 			New Game
 		</button>
@@ -81,7 +85,7 @@
 		<h1>NEW GAME</h1>
 		{#each players as player}
 			<div>
-				<input bind:value={player.name}>
+				<input class="player-name" bind:value={player.name}>
 			</div>
 		{/each}
 		<button on:click={changeStatus('GAME_START')}>GO</button>
@@ -89,33 +93,33 @@
 	{#if gameStatus == 'GAME_START'}
 		{#each players as player}
 			<div class="player-score">
-				<p>{player.name}</p>
-				<p>{player.score}</p>
+				<div>{player.name}</div>
+				<div style="font-size: 40px;">{player.score}</div>
 			</div>
 		{/each}
 		<button on:click={changeStatus('GAME_ADD_RESULT')}>ADD RESULT</button>
 	{/if}
 	{#if gameStatus == 'GAME_ADD_RESULT'}
 		<h3>Winner</h3>
-		<div style="display: grid; grid-template-columns: 1fr 1fr 1fr 1fr;">
+		<div class="button-group winner">
 			{#each players as player, i}
-				<button on:click={setWinner(i)} style="background-color: {winnerIndex === i ? "yellow" : "#f4f4f4"}">{player.name}</button>
+				<button on:click={setWinner(i)} style="background-color: {winnerIndex === i ? "gold" : "#f4f4f4"}">{player.name}</button>
 			{/each}
 		</div>
 		<h3>Points</h3>
-		<div style="display: grid; grid-template-columns: 1fr 1fr 1fr 1fr 1fr;">
-			<button on:click={setPoints(1)} style="background-color: {points === 1 ? "yellow" : "#f4f4f4"}">1</button>
-			<button on:click={setPoints(2)} style="background-color: {points === 2 ? "yellow" : "#f4f4f4"}">2</button>
-			<button on:click={setPoints(3)} style="background-color: {points === 3 ? "yellow" : "#f4f4f4"}">3</button>
-			<button on:click={setPoints(4)} style="background-color: {points === 4 ? "yellow" : "#f4f4f4"}">4</button>
-			<button on:click={setPoints(5)} style="background-color: {points === 5 ? "yellow" : "#f4f4f4"}">5</button>
+		<div class="button-group points">
+			<button on:click={setPoints(1)} style="background-color: {points === 1 ? "gold" : "#f4f4f4"}">1</button>
+			<button on:click={setPoints(2)} style="background-color: {points === 2 ? "gold" : "#f4f4f4"}">2</button>
+			<button on:click={setPoints(3)} style="background-color: {points === 3 ? "gold" : "#f4f4f4"}">3</button>
+			<button on:click={setPoints(4)} style="background-color: {points === 4 ? "gold" : "#f4f4f4"}">4</button>
+			<button on:click={setPoints(5)} style="background-color: {points === 5 ? "gold" : "#f4f4f4"}">5</button>
 		</div>
 		<h3>Loser</h3>
-		<div style="display: grid; grid-template-columns: 1fr 1fr 1fr 1fr;">
+		<div class="button-group loser">
 			{#each players as player, i}
 				<button
 					on:click={setLoser(i)}
-					style="background-color: {loserIndex === i ? "red" : "#f4f4f4"}"
+					style="background-color: {loserIndex === i ? "gold" : "#f4f4f4"}"
 					disabled={winnerIndex === i}
 				>
 					{player.name}
@@ -128,24 +132,63 @@
 </main>
 
 <style>
+	@import url('https://fonts.googleapis.com/css?family=Rubik');
+
 	main {
-		text-align: center;
 		padding: 1em;
-		max-width: 50vw;
-		margin: 0 auto;
+		width: 70%;
+		font-family: 'Rubik';
 	}
 
 	h1 {
-		color: #ff3e00;
 		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
+		font-size: 40px;
+		text-align: left;
 	}
 
 	.player-score {
-		display: flex;
-		flex-direction: row;
-		justify-content: space-between;
+		margin-bottom: 15px;
 	}
 
+	button {
+		padding: 10px 20px;
+		border-radius: 10px;
+		background-color: gold;
+		font-size: 20px;
+		text-transform: uppercase;
+		width: 100%;
+		margin: 10px 0;
+		border: none;
+		text-overflow: ellipsis;
+	}
+
+	input.player-name{
+		width: 100%;
+		border-radius: 10px;
+		padding: 10px 15px;
+	}
+
+	.button-group {
+		display: grid;
+		grid-template-columns: 1fr 1fr;
+	}
+
+	.button-group.loser {
+		margin-bottom: 20px;
+	}
+
+	.button-group.points {
+		grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+	}
+
+	.button-group button {
+		margin: 0;
+		height: 100%;
+		border-radius: 0;
+		padding: 10px;
+	}
+	h3 {
+		margin-top: 20px;
+		margin-bottom: 10px;
+	}
 </style>
